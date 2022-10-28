de7547043af22a3e20c4b287e5e48f878d5c589e
Rahul <rahulgore575@gmail.com>
2022-10-28 15:39:26 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,7 +28,9 @@ const Navbar = () => {
         <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Link href='/#main'>
+                    <>
                     <Image className='cursor-pointer' src="/../public/assets/logo3.png" alt="/" width='80' height='50' />
+                    </>
                 </Link>
 
                 <div>

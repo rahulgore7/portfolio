c57e0024a40fb13549184def9429a871b9c5c78d
Rahul Gore <107752614+rahulgore7@users.noreply.github.com>
2023-12-20 10:30:47 +0530

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
                     <p className='uppercase text-sm tracking-widest text-gray-600'>Let's Build Something Together</p>
                     <h1 className='py-4 text-gray-700'>Hi,I'm <span className='text-[#5651e5]'>Rahul</span></h1>
                     <h1 className='py-4 text-gray-700'>A Full Stack Web Developer</h1>
-                    <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm a front-end web developer specializing in building (and ocassionally designing) exceptional digital experiences. Currently I'm focused on building responsive front-end web applications while learning back-end technologies.</p>
+                    <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm a full stack web developer specializing in building (and ocassionally designing) exceptional digital experiences. Currently I'm focused on building responsive front-end web applications while learning back-end technologies.</p>
 
                     <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                         <Link href='https://www.linkedin.com/in/rahul-gore-563029228/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B%2Fe8SKqW3TdeYp8Eu4OQnUg%3D%3D' >

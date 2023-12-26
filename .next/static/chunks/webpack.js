2f44b857ebda593eaf83427c028362c1ab24d9c4
Rahul Gore <rahulgore575@gmail.com>
2023-12-26 11:59:07 +0530

    ecom project added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "de18e940addcfb88"; }
+/******/ 		__webpack_require__.h = function() { return "aa1cc8b37db672f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,35 +1,44 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import rudra from '../public/assets/rudra.jpg'
-import ProjectItems from './ProjectItems'
-import starbucks from '../public/assets/starbucks.jpg'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import rudra from "../public/assets/rudra.jpg";
+import ProjectItems from "./ProjectItems";
+import starbucks from "../public/assets/starbucks.jpg";
+import ecom from "../public/assets/projects/ecom.png";
 
 const Projects = () => {
-    return (
-        <div id='projects' className='w-full'>
-            <div className='max-w-[1240px] mx-auto px-2 py-16'>
-                <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
-                    Projects
-                </p>
-                <h2 className='py-4'>
-                    What I've Built
-                </h2>
-                <div className='grid md:grid-cols-2 gap-8'>
-                    <ProjectItems title='Blogging Website'
-                        backgroundImg={rudra}
-                        type='NextJs | TailWindCSS | GraphCMS'
-                        projectUrl='https://rudra-rahulgore7.vercel.app/' />
+  return (
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+        <ProjectItems
+            title="ShopKart: A MERN Stack Ecommerce Platform"
+            backgroundImg={ecom}
+            type="React | Express | Node | MongoDB "
+            projectUrl="https://ecomxecom.vercel.app/"
+          />
+          <ProjectItems
+            title="Blogging Website"
+            backgroundImg={rudra}
+            type="NextJs | TailWindCSS | GraphCMS"
+            projectUrl="https://rudra-rahulgore7.vercel.app/"
+          />
 
-                    <ProjectItems title='Starbucks Landing Page'
-                        backgroundImg={starbucks}
-                        type='HTML | CSS | JavaScript'
-                        projectUrl='https://rahulgore7.github.io/Starbucks-Landing/' />
-                </div>
-            </div>
-
+          <ProjectItems
+            title="Starbucks Landing Page"
+            backgroundImg={starbucks}
+            type="HTML | CSS | JavaScript"
+            projectUrl="https://rahulgore7.github.io/Starbucks-Landing/"
+          />
+          
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
